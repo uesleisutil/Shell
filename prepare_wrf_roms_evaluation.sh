30a872372ab450b7eb0534eb748bf8a977afa5f0
Ueslei Sutil <ueslei@outlook.com>
2019-04-15 09:10:50 -0300

    Update prepare_wrf_roms_evaluation.sh

--- prepare_wrf_roms_evaluation.sh
+++ prepare_wrf_roms_evaluation.sh
@@ -4,7 +4,7 @@
 # Email:          uesleisutil1@gmail.com
 # Created:        12 April 2019
 # Last modified:  12 April 2019
-# Version:        1.0
+# Version:        1.1
 #
 # Creates NetCDF files from ROMS and WRF outputs with same 
 # time-steps from the following data and reanalysis:
@@ -26,18 +26,20 @@
 # Prepare ROMS.
 cd /media/ueslei/Ueslei/INPE/PCI/SC_2008/Outputs/normal/nonudg
 ncks -v temp,u,v -d s_rho,49,49 roms_nonudg.nc roms_nonudg_sfc.nc
-cdo daymean roms_nonudg_sfc.nc roms_nonudg_daily.nc
-cdo splitday roms_nonudg_daily.nc day
+cdo splitday roms_nonudg_sfc.nc day
 cdo cat day21.nc day26.nc roms_nonudg_21_26_days_oscar.nc
-rm -rf day* roms_nonudg_sfc.nc
-mv roms_nudg_daily.nc roms_nonudg_21_26_days_oscar.nc /media/ueslei/Ueslei/INPE/PCI/SC_2008/Evaluation/ROMS
+cdo cat day20.nc day21.nc day22.nc day23.nc day24.nc day25.nc day26.nc day27.nc day28.nc day29.nc day29.nc day30.nc roms_nonudg_sfc_split.nc 
+cdo daymean roms_nonudg_sfc_split.nc roms_nonudg_daily.nc
+rm -rf day* roms_nonudg_sfc.nc roms_nonudg_sfc_split.nc
+mv roms_nonudg_daily.nc roms_nonudg_21_26_days_oscar.nc /media/ueslei/Ueslei/INPE/PCI/SC_2008/Evaluation/ROMS
 
 cd /media/ueslei/Ueslei/INPE/PCI/SC_2008/Outputs/normal/nudg
 ncks -v temp,u,v -d s_rho,49,49 roms_nudg.nc roms_nudg_sfc.nc
-cdo daymean roms_nudg_sfc.nc roms_nudg_daily.nc
-cdo splitday roms_nudg_daily.nc day
+cdo splitday roms_nudg_sfc.nc day
 cdo cat day21.nc day26.nc roms_nudg_21_26_days_oscar.nc
-rm -rf day* roms_nudg_sfc.nc
+cdo cat day20.nc day21.nc day22.nc day23.nc day24.nc day25.nc day26.nc day27.nc day28.nc day29.nc day29.nc day30.nc roms_nudg_sfc_split.nc 
+cdo daymean roms_nudg_sfc_split.nc roms_nudg_daily.nc
+rm -rf day* roms_nudg_sfc.nc roms_nudg_sfc_split.nc
 mv roms_nudg_daily.nc roms_nudg_21_26_days_oscar.nc /media/ueslei/Ueslei/INPE/PCI/SC_2008/Evaluation/ROMS
 
 # Prepare WRF.
